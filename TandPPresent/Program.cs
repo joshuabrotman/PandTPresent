7ed1479c0f4f2a22d62d92c3b0c9c823346fb85d
magnetoplasmadynamics <link1922@gmail.com>
2020-03-14 15:32:08 -0500

    message

---
+++ TandPPresent/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TandPPresent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int t = 0;
+            int p = 0;
+            Console.WriteLine("Please enter a string containing p and t characters.");
+            string userInput = Console.ReadLine();
+
+            foreach(char i in userInput)
+            {
+                if (i == 't') { t++; }
+                if (i == 'p') { p++; }
+            }
+
+            Console.WriteLine("Total T's:" + t);
+            Console.WriteLine("Total P's:" + p);
+            Console.ReadKey();
+
+        }
+    }
+}
